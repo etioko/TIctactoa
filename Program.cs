459a118393ec21eb682ad3e351a562bbbaf82588
etioko <okonsky3@gmail.com>
2021-04-06 15:06:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+
+namespace Tictactoa
+{
+    class Program
+    {
+
+        static int player, countMoves, boarderSize;
+        static int[] answerPlayer = new int[2];
+        static bool startOver = true;
+        static XOBoard board;
+
+        static void Main(string[] args)
+        {
+            while (startOver == true)
+            {
+                Console.Clear();
+                player = 1;
+                countMoves = 0;
+                boarderSize = 0;
+                Console.WriteLine("Welcome to tictactoa game!\n");
+                createBoard();
+                Console.WriteLine("Do you want to play again ? [yes|no]");
+                string again = Console.ReadLine();
+                if (again == "NO" || again == "No" || again == "no")
+                    startOver = false;
+            }
+        }
+
+        private static void createBoard()
+        {
+            int size = -1;
+            while (size <= 1)
+            {
+                Console.WriteLine("please enter number that will be the board size(numberXnumber)");
+                size = checkAnswer(Console.ReadLine()); //return -1 if the answer is wrong
+
+            }
+            board = new XOBoard(size);
+            boarderSize = size;
+
+            board.Draw();
+            Console.WriteLine("Player1:X and Player2:O \n");
+            startGame();
+        }
+
+        private static void startGame()
+        {
+            string answer = "";
+            Console.WriteLine("Please answer in the following format: N,N");
+
+            if (player % 2 == 0)
+            {
+                Console.Write("Player 2 Chance: ");
+            }
+            else
+            {
+                Console.Write("Player 1 Chance: ");
+            }
+            bool flag = false;
+            while (flag == false)
+            {
+                Console.WriteLine("In which ROW,COLUMN do you put your answer");
+                answer = Console.ReadLine();
+                flag = rowColumn(answer); //where to put your char
+                if (flag == true && !board.Put(answerPlayer, player))
+                {
+                    Console.WriteLine("this place is already taken");
+                    flag = false;
+                }
+
+            }
+            board.Draw();
+            countMoves++;
+
+            if (countMoves > boarderSize)
+            {
+                string win = board.Status(countMoves);
+                if (win == "NONE")
+                {
+                    if (player % 2 == 0) player = 1;
+                    else
+                        player = 2;
+                    startGame();
+                }
+                else
+                    Console.WriteLine("#### " + win + "####");
+
+            }
+            else
+            {
+
+                if (player % 2 == 0) player = 1;
+                else
+                    player = 2;
+                startGame();
+            }
+        }
+
+        private static bool rowColumn(string answer)
+        {
+            string[] splits = answer.Split(',');
+            if (splits.Length < 2) //check that you entered only two numbers
+            {
+                return false;
+            }
+            for (int i = 0; i < splits.Length; i++)
+            {
+                int num = checkAnswer(splits[i]);
+                if (num == -1)
+                    return false;
+                if (!Enumerable.Range(1, boarderSize).Contains(num))
+                    return false;
+                else
+                    answerPlayer[i] = num;
+
+            }
+            return true;
+        }
+
+        private static int checkAnswer(string numberStr)
+        {
+            try
+            {
+                int num = Int16.Parse(numberStr);
+                return num;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The format of '{0}' is invalid.", numberStr);
+                return -1;
+
+            }
+        }
+    }
+}
---
+++ XOBoard.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace Tictactoa
+{
+    class XOBoard
+    {
+        private int boardSize;
+
+
+        public int BoardSize
+        {
+            get { return boardSize; }
+            set { boardSize = value; }
+        }
+
+
+        char[,] gameBoard;
+
+        public XOBoard()
+        {
+        }
+
+        public XOBoard(int size)
+        {
+            this.BoardSize = size;
+            gameBoard = new char[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    gameBoard[i, j] = '-';
+                }
+            }
+        }
+
+
+        //position = where to put X or O 
+        public bool Put(int[] position, int player)
+        {
+            char x_or_o;
+            if (player % 2 == 1)
+                x_or_o = 'X';
+            else
+                x_or_o = '0';
+            if (gameBoard[position[0] - 1, position[1] - 1] == '-')
+            {
+                gameBoard[position[0] - 1, position[1] - 1] = x_or_o;
+                return true;
+            }
+            return false;
+
+        }
+
+        public string Status(int countMoves)
+        {
+            //check - rows
+            int rows = 0, count_same_char;
+            char first;
+            first = gameBoard[rows, 0];
+            if (first != '-')
+            {
+                while (rows < boardSize)
+                {
+                    count_same_char = 0;
+                    for (int index = 1; index < boardSize; index++)
+                    {
+                        if (first != gameBoard[rows, index])
+                            index = boardSize;
+                        else
+                            count_same_char++;
+                    }
+                    if (count_same_char == boardSize - 1)
+                        return first + " WIN!! Row: " + (rows + 1);
+                    else
+                        rows++;
+                }
+            }
+            //check columns:
+            int col = 0;
+            first = gameBoard[0, col];
+            if (first == '-')
+            {
+                while (col < boardSize)
+                {
+                    count_same_char = 0;
+
+                    for (int index = 1; index < boardSize; index++)
+                    {
+                        if (first != gameBoard[index, col])
+                            index = boardSize;
+                        else
+                            count_same_char++;
+                    }
+                    if (count_same_char == boardSize - 1)
+                        return first + " WIN!! coloumn: " + col + 1;
+                    else
+                        col++;
+                }
+            }
+            //Main diagonal
+            first = gameBoard[0, 0];
+            if (first != '-')
+            {
+                count_same_char = 0;
+                for (int i = 1; i < boardSize; i++)
+                {
+                    if (first != gameBoard[i, i])
+                        i = boardSize;
+                    else
+                        count_same_char++;
+
+                }
+                if (count_same_char == BoardSize - 1)
+                    return first + " WIN!! Main diagonal ";
+            }
+
+            //Secondary diagonal
+            first = gameBoard[0, boardSize - 1];
+            if (first != '-')
+            {
+                int second = boardSize - 2;
+                count_same_char = 0;
+                for (int i = 1; i < boardSize; i++)
+                {
+                    if (first != gameBoard[i, second])
+                        i = boardSize;
+                    else
+                    {
+                        second--;
+                        count_same_char++;
+                    }
+                }
+                if (count_same_char == BoardSize - 1)
+                    return first + " WIN!! Secondary diagonal ";
+            }
+            if (countMoves == BoardSize * boardSize)
+            {
+                int countDraw = 0;
+                //CHECK IF DRAW:
+                for (int i = 0; i < boardSize; i++)
+                {
+                    for (int j = 0; j < boardSize; j++)
+                    {
+                        if (gameBoard[i, j] == '-')
+                            countDraw++;
+                    }
+                }
+                if (countDraw == 0)
+                    return "DRAW!";
+            }
+                return "NONE";
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("\n\n");
+            for (int i = 0; i < BoardSize; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Console.Write("     " + gameBoard[i, j]);
+                }
+                Console.WriteLine("     |");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+
+    }
+}
